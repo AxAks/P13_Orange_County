95fa70379b7bc1be6bdb9f6e1a2cf3ff28cf21bc
ax <axel.akonde@cinego.net>
2022-07-01 17:19:40 +0200

    Sentry: DSN moved in .env, test error handled

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
-from django.contrib import messages
 
 
-def trigger_error(request):  # just for tests
+def trigger_error(request):
+    """
+    just for tests
+    """
     try:
         division_by_zero = 1 / 0
     except ZeroDivisionError as e:
