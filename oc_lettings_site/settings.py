aa23785b24ecf527efebe3719942e1407d2bd3f7
ax <axel.akonde@cinego.net>
2022-07-01 16:47:34 +0200

    Sentry, ajout dans requirements, integration et première erreur de test

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,8 +1,25 @@
 import os
-
+import sentry_sdk
 from dotenv import load_dotenv
+from sentry_sdk.integrations.django import DjangoIntegration
 
 load_dotenv()
+
+sentry_sdk.init(
+    dsn="https://862a82ab58464080a41e1107ace4504c@o1289316.ingest.sentry.io/6507673",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+def trigger_error(request):  # just for tests
+    try:
+        division_by_zero = 1 / 0
+    except ZeroDivisionError as e:
+        print('hello Zero')
+        raise ZeroDivisionError(e)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace='home')),
     path('profiles/', include('profiles.urls', namespace='profiles')),
     path('lettings/', include('lettings.urls', namespace='lettings')),
+    path('sentry-debug/', trigger_error)  # just for tests
 ]
