a23ae2aa155ee238550634ef9c7adf752ff5ebcd
Aksel <axel.akonde@gmail.com>
2022-07-22 06:13:42 +0200

    Heroku staticfiles in static without whitenoise but STATICFILES_DIRS in settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -60,7 +60,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'oc_lettings_site.urls'
@@ -138,4 +137,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
